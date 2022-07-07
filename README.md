8eff3b07e3744ad52f98423f029a8d2a380fd3fe
bearlord <565364226@qq.com>
2022-07-07 18:02:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PDO_TAOS(W) Helper
+
+This package contains IDE help files for PDO_TAOS and PDO_TAOSW. You may use it in your IDE to provide accurate autocompletion.
---
+++ src/PDO.php
@@ -0,0 +1,69 @@
+<?php
+
+class PDO
+{
+    const PARAM_TAOS_NULL = 6000;
+
+    const PARAM_TAOS_BOOL = 6001;
+
+    const PARAM_TAOS_TINYINT = 6002;
+
+    const PARAM_TAOS_SMALLINT = 6003;
+
+    const PARAM_TAOS_INT = 6004;
+
+    const PARAM_TAOS_BIGINT = 6005;
+
+    const PARAM_TAOS_FLOAT = 6006;
+
+    const PARAM_TAOS_DOUBLE = 6007;
+
+    const PARAM_TAOS_BINARY = 6008;
+
+    const PARAM_TAOS_TIMESTAMP = 6009;
+
+    const PARAM_TAOS_NCHAR = 6010;
+
+    const PARAM_TAOS_UTINYINT = 6011;
+
+    const PARAM_TAOS_USMALLINT = 6012;
+
+    const PARAM_TAOS_UINT = 6013;
+
+    const PARAM_TAOS_UBIGINT = 6014;
+
+    const PARAM_TAOS_JSON = 6015;
+
+
+    const PARAM_TAOSW_NULL = 6000;
+
+    const PARAM_TAOSW_BOOL = 6001;
+
+    const PARAM_TAOSW_TINYINT = 6002;
+
+    const PARAM_TAOSW_SMALLINT = 6003;
+
+    const PARAM_TAOSW_INT = 6004;
+
+    const PARAM_TAOW_BIGINT = 6005;
+
+    const PARAM_TAOSW_FLOAT = 6006;
+
+    const PARAM_TAOSW_DOUBLE = 6007;
+
+    const PARAM_TAOSW_BINARY = 6008;
+
+    const PARAM_TAOSW_TIMESTAMP = 6009;
+
+    const PARAM_TAOSW_NCHAR = 6010;
+
+    const PARAM_TAOSW_UTINYINT = 6011;
+
+    const PARAM_TAOSW_USMALLINT = 6012;
+
+    const PARAM_TAOSW_UINT = 6013;
+
+    const PARAM_TAOSW_UBIGINT = 6014;
+
+    const PARAM_TAOSW_JSON = 6015;
+}
